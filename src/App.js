09315859c363f454426f82e37113977b5022755c
inseob jeon <pravda@pravdalaptop-2.local>
2020-08-26 14:46:17 +0900

    making reducers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import { ADD_TODO,
+         TOGGLE_TODO,
+         VisibilityFilters, 
+         SET_VISIBILITY_FILTER
+        } from './actions'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      })
+    default:
+        return state;
+  }
+}
 
 function App() {
   return (
