89a4933a940dbcd9f40e32be5f4457d653232424
inseob jeon <pravda@pravdalaptop-2.local>
2020-08-26 15:12:19 +0900

    split reducer : handling only visibility filter state and handling only todo(task) and it's state

--- src/App.js
+++ src/App.js
@@ -10,6 +10,30 @@ const initialState = {
   todos: []
 }
 
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
 function todoApp(state = initialState, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
@@ -18,16 +42,14 @@ function todoApp(state = initialState, action) {
       })
     case ADD_TODO:
       return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
+        todos: todos(state.todos, action)
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
       })
     default:
-        return state;
+      return state
   }
 }
 
